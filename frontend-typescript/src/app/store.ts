ae97c986ec88e89dc8808d54eb8394cf3d9f2ddc
azdrogov <azdrogov@naumen.ru>
2022-10-17 21:32:31 +0500

    Revert "Продолжаем переход на TypeScript"
    This reverts commit 5832853de476c7745df86ace93f984d751fb2e62.

--- frontend-typescript/src/app/store.ts
+++ frontend-typescript/src/app/store.ts
@@ -1,11 +1,6 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import initialState from './state/InitialState';
-import {createEpicMiddleware} from "redux-observable";
 
-const initialState1 = initialState
-
-const epicMiddleware = createEpicMiddleware();
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
--- frontend-typescript/src/components/Tutorials.tsx
+++ frontend-typescript/src/components/Tutorials.tsx
@@ -1,7 +1,1 @@
-import {Tutorials} from '../app/state/StoreNamespace';
 
-const Tutorials = (props) => {
-    tutorials: Tutorials
-}
-
-export default Tutorials;
